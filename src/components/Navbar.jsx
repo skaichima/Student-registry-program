d4e4c371f333e1eb07a72325091587ef606be5f4
skaichima <skaichima@gmail.com>
2023-02-19 15:32:44 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from'react'
+
+function Navbar() {
+    return (
+        <nav className="bg-white p-3">
+            <span className="material-symbols-outlined text-3xl">Leaderboard</span>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/components/data.jsx
@@ -0,0 +1,44 @@
+export default [
+    {
+        id: 1,
+        name: "Chimaobi Chukwuegbo",
+        grade: "A",
+        description: "A frontend web developer currently undergoing an internship with genesys tech hub.",
+        imageUrl: "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    },
+    {
+        id: 2,
+        name: "Mount Fuji",
+        grade: "A",
+        description: "A frontend web developer currently undergoing an internship with genesys tech hub.",
+        imageUrl: "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        id: 3,
+        name: "Mount Fuji",
+        grade: "A",
+        description: "A frontend web developer currently undergoing an internship with genesys tech hub.",
+        imageUrl: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        id: 4,
+        name: "Mount Fuji",
+        grade: "A",
+        description: "A frontend web developer currently undergoing an internship with genesys tech hub.",
+        imageUrl: "https://images.unsplash.com/photo-1530268729831-4b0b9e170218?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA1fHxwZXJzb258ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        id: 5,
+        name: "Mount Fuji",
+        grade: "A",
+        description: "A frontend web developer currently undergoing an internship with genesys tech hub.",
+        imageUrl: "https://images.unsplash.com/photo-1525875975471-999f65706a10?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwZXJzb258ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        id: 6,
+        name: "Mount Fuji",
+        grade: "A",
+        description: "A frontend web developer currently undergoing an internship with genesys tech hub.",
+        imageUrl: "https://images.unsplash.com/photo-1569931727733-7fe2e67afe1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjk2fHxwZXJzb258ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+    },
+]
